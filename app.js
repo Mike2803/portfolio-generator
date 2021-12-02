2d7424cc477e86a43a6a4a0be8494aaae3d765fa
michael coleman <colemanmichael671@gmail.com>
2021-12-02 15:26:53 -0500

    modularize the code

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
+const generatePage = require('./src/template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
+/*const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};*/
 
+fs.writeFile('index.html', generatePage(profileDataArgs[0], profileDataArgs[1]), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
 
-
-
-
-
-
-
-
-const printProfileData = (profileDataArr) => {
+/*const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
     }
@@ -24,4 +42,4 @@ const printProfileData = (profileDataArr) => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
